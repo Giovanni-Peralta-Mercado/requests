78a4dd01631bfdc800451dcc510675c7794a964f
Giovanni-Peralta-Mercado <isc20350543@gmail.com>
2023-09-21 02:57:11 -0600

    Query params

--- index.js
+++ index.js
@@ -15,6 +15,29 @@ app.get('/:user/:password',(req,res)=>{
     res.status(404).json({msg: 'Error en el usuario o contraseña'})
 })
 
+//  http://localhost:3000/login?user=giovanni&&password=1234
+app.get('/login',(req,res)=>{  //Endpoint
+    const {user,password} = req.query
+    if(!user || !password){
+        res.status(400).json({
+            msg:'You need to provide <user> and <password> parameters'
+        })
+        return
+
+    }
+
+
+    if(user=== 'giovanni' && password ==='1234'){
+        res.json({msg: 'Inicio de sesion exitoso'})
+        return
+    }
+
+
+    res.status(404).json({msg: 'Error en el usuario o contraseña'})
+
+
+})
+
 
 app.post('/',(request,response)=>{
     response.json({msg:'Hola Post'})

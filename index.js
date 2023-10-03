5897e89bbffa7d967de0685b31308cefede61447
Giovanni-Peralta-Mercado <isc20350543@gmail.com>
2023-10-03 00:53:40 -0600

    requests

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 
 const app = express()
+app.use(express.json())
 
 app.get('/:user/:password',(req,res)=>{
 
@@ -35,6 +36,28 @@ app.get('/login',(req,res)=>{  //Endpoint
 
     res.status(404).json({msg: 'Error en el usuario o contraseña'})
 
+
+})
+
+//  http://localhost:3000/login
+app.post('/login',(req,res)=>{
+    const {user,password} = req.body
+    if(!user || !password){
+        res.status(400).json({
+            msg:'You need to provide <user> and <password> parameters'
+        })
+        return
+
+    }
+
+
+    if(user=== 'giovanni' && password ==='1234'){
+        res.json({msg: 'Inicio de sesion exitoso'})
+        return
+    }
+
+
+    res.status(404).json({msg: 'Error en el usuario o contraseña'})
 
 })
 
